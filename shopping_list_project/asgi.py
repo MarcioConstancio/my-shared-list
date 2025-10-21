70410063e504ffa721be9bff2c889cc3b949083a
MarcioConstancio <marciocjr.13@gmail.com>
2025-10-20 22:43:46 -0300

    corrected shopping_list_project/asgy.py

--- shopping_list_project/asgi.py
+++ shopping_list_project/asgi.py
@@ -1,13 +1,18 @@
 import os
 from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shopping_list_project.settings')
+application = get_asgi_application()
+
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import core.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shopping_list_project.settings')
+
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": application,
     "websocket": AuthMiddlewareStack(
         URLRouter(
             core.routing.websocket_urlpatterns
